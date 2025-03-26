ac547c99a9f129cce4845bd4ba3439fb7b15a2da
Matthias Moser <mmoser@united-internet.de>
2025-03-26 12:58:11 +0100

    small changes

--- src/CorrelationIdBundle.php
+++ src/CorrelationIdBundle.php
@@ -27,6 +27,7 @@ class CorrelationIdBundle extends AbstractBundle {
      */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+        $container->import('../config/services.yaml');
 //        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__ . '/../config'));
 //        $loader->load('correlationId.yaml');
     }
